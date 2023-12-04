0024eb033036caae73e5edf0d9211f2643992c51
joricky91 <jonathanricky91@gmail.com>
2023-12-04 19:04:50 +0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Jonathan Ricky Sandjaja on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
